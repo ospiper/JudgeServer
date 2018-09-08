81e4784558b9e0638108b44378701c3fdce76d8d
LLAP-OMEN <ospiper@foxmail.com>
2018-09-08 20:55:22 +0800

    modified for apoj client

--- client/Python/client.py
+++ client/Python/client.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
-
+import pika
+import sys
 import requests
 
 from languages import c_lang_config, cpp_lang_config, java_lang_config, c_lang_spj_config, \
@@ -49,97 +50,115 @@ class JudgeServerClient(object):
         return self._request(self.server_base_url + "/compile_spj", data=data)
 
 
-if __name__ == "__main__":
-    token = "YOUR_TOKEN_HERE"
+def judge(problemID, lang, code, memoryLimit, timeLimit):
+    lang_config = java_lang_config
+    if lang == 'c':
+        lang_config = c_lang_config
+    elif lang == 'cpp':
+        lang_config = cpp_lang_config
+    elif lang == 'py2':
+        lang_config = py2_lang_config
+    elif lang == 'py3':
+        lang_config = py3_lang_config
+    return client.judge(
+        src=code,
+        language_config=lang_config,
+        max_cpu_time=timeLimit,
+        max_memory=memoryLimit,
+        test_case_id=problemID,
+        output=False
+    )
 
-    c_src = r"""
-    #include <stdio.h>
-    int main(){
-        int a, b;
-        scanf("%d%d", &a, &b);
-        printf("%d\n", a+b);
-        return 0;
-    }
-    """
 
-    c_spj_src = r"""
-    #include <stdio.h>
-    int main(){
-        return 1;
-    }
-    """
+def consume(ch, method, properties, body):
+    data = json.loads(body)
+    print(data)
 
-    cpp_src = r"""
-    #include <iostream>
-
-    using namespace std;
-
-    int main()
-    {
-        int a,b;
-        cin >> a >> b;
-        cout << a+b << endl;
-        return 0;
-    }
-    """
-
-    java_src = r"""
-    import java.util.Scanner;
-    public class Main{
-        public static void main(String[] args){
-            Scanner in=new Scanner(System.in);
-            int a=in.nextInt();
-            int b=in.nextInt();
-            System.out.println(a + b);
+    submitID = data['submitID']
+    problemID = data['problemID']
+    preJudge = requests.post('http://oj.ll-ap.cn:3000/judger/start',
+                             json={'submitID': submitID, 'judger': 'HKReporter'})
+    preJudgeData = None
+    try:
+        preJudgeData = preJudge.json()
+    except Exception:
+        preJudgeData = {
+            'err': 'Cannot get problem status',
         }
-    }
-    """
+    memoryLimit = 128 * 1024 * 1024
+    timeLimit = 1000
+    if preJudgeData['err'] == '':
+        memoryLimit = preJudgeData['memoryLimit']
+        timeLimit = preJudgeData['timeLimit']
+    judge_data = judge(problemID, data['compiler'], data['code'], memoryLimit, timeLimit)
+    # print(judge_data)
+    if not judge_data['err'] is None:
+        # print("ERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR")
+        ret = {
+            'judger': 'HKReporter',
+            'score': 0,
+            'status': 6,
+            'peakMemory': 0,
+            'runtime': 0,
+            'err': judge_data['data'],
+            'results': None,
+            'hash': '19260817'
+        }
+    else:
+        # print("SUCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC")
+        _score = 0
+        _status = 0
+        _time = 0
+        _memory = 0
+        _avgscore = 100 / len(judge_data['data'])
+        for item in judge_data['data']:
+            _time = _time + item['cpu_time']
+            _memory = max(_memory, item['memory'])
+            if item['result'] == 0:
+                _score = _score + _avgscore
+            else:
+                if _status == 0:
+                    _status = item['result']
 
-    py2_src = """s = raw_input()
-s1 = s.split(" ")
-print int(s1[0]) + int(s1[1])"""
+        ret = {
+            'judger': 'HKReporter',
+            'score': _score,
+            'status': _status,
+            'peakMemory': _memory,
+            'runtime': _time,
+            'err': judge_data['err'],
+            'results': judge_data['data'],
+            'hash': '19260817'
+        }
+    print(ret)
+    requests.post('http://oj.ll-ap.cn:3000/judger/judge/%s' % (submitID), json=ret)
+    channel.basic_ack(delivery_tag=method.delivery_tag)
 
-    py3_src = """s = input()
-s1 = s.split(" ")
-print(int(s1[0]) + int(s1[1]))"""
 
+if __name__ == "__main__":
+    token = "apoj"
     client = JudgeServerClient(token=token, server_base_url="http://127.0.0.1:12358")
-    print("ping")
-    print(client.ping(), "\n\n")
 
-    print("compile_spj")
-    print(client.compile_spj(src=c_spj_src, spj_version="2", spj_compile_config=c_lang_spj_compile
-                             ), "\n\n")
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            '118.89.102.93',
+            credentials=pika.credentials.PlainCredentials(
+                username='apoj',
+                password='t3dv95my'
+            ), socket_timeout=5
+        )
+    )
+    channel = connection.channel()
+    channel.queue_declare(
+        queue="apoj_submit_queue",
+        durable=True
+    )
 
-    print("c_judge")
-    print(client.judge(src=c_src, language_config=c_lang_config,
-                       max_cpu_time=1000, max_memory=1024 * 1024 * 128,
-                       test_case_id="normal", output=True), "\n\n")
-
-    print("cpp_judge")
-    print(client.judge(src=cpp_src, language_config=cpp_lang_config,
-                       max_cpu_time=1000, max_memory=1024 * 1024 * 128,
-                       test_case_id="normal"), "\n\n")
-
-    print("java_judge")
-    print(client.judge(src=java_src, language_config=java_lang_config,
-                       max_cpu_time=1000, max_memory=256 * 1024 * 1024,
-                       test_case_id="normal"), "\n\n")
-
-    print("c_spj_judge")
-    print(client.judge(src=c_src, language_config=c_lang_config,
-                       max_cpu_time=1000, max_memory=1024 * 1024 * 128,
-                       test_case_id="spj",
-                       spj_version="3", spj_config=c_lang_spj_config,
-                       spj_compile_config=c_lang_spj_compile, spj_src=c_spj_src), "\n\n")
-
-
-    print("py2_judge")
-    print(client.judge(src=py2_src, language_config=py2_lang_config,
-                       max_cpu_time=1000, max_memory=128 * 1024 * 1024,
-                       test_case_id="normal"), "\n\n")
-
-    print("py3_judge")
-    print(client.judge(src=py3_src, language_config=py3_lang_config,
-                       max_cpu_time=1000, max_memory=128 * 1024 * 1024,
-                       test_case_id="normal"), "\n\n")
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(consume, queue='apoj_submit_queue')
+    print('[INFO] Successfully started consuming... Press Ctrl+C to exit.')
+    try:
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        print('Keyboard Interrupted. Closing connection...')
+        connection.close()
